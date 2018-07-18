ec988eb273a0538b368daccda4f161b6f011ee31
parkmeta <mrmeta3965@gmail.com>
2018-07-18 20:47:20 +0900

    Temporary saved

--- drawingboardview/src/main/java/com/mrmeta/drawingboardview/DrawingBoardView.java
+++ drawingboardview/src/main/java/com/mrmeta/drawingboardview/DrawingBoardView.java
@@ -2,20 +2,24 @@ package com.mrmeta.drawingboardview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 
-import com.mukesh.DrawingView;
-
-public class DrawingBoardView extends DrawingView {
+public class DrawingBoardView extends View {
 
     public DrawingBoardView(Context c) {
         super(c);
     }
 
     public DrawingBoardView(Context c, AttributeSet attrs) {
-        super(c, attrs);
+        super(c, attrs, 0);
     }
 
     public DrawingBoardView(Context c, AttributeSet attrs, int defStyle) {
         super(c, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        inflate(getContext(), R.layout.view_drawing_board, null);
     }
 }
