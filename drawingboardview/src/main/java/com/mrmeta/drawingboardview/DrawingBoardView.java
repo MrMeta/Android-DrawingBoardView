e315aac37a9df56e12e5aafd5c26b3a424c78142
MrMeta <bungyo92@gmail.com>
2018-07-21 14:19:16 +0900

    Make that :drawingboardview works as a library

---
+++ drawingboardview/src/main/java/com/mrmeta/drawingboardview/DrawingBoardView.java
@@ -0,0 +1,21 @@
+package com.mrmeta.drawingboardview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.byox.drawview.views.DrawView;
+
+class DrawingBoardView extends DrawView {
+
+    public DrawingBoardView(Context context) {
+        super(context);
+    }
+
+    public DrawingBoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DrawingBoardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
